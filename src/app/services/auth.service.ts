12073c417a656c46ad4fe57adfc3f79d35c015bd
VXSnipe <mattsheld01@gmail.com>
2026-01-16 17:13:40 +0100

    Add environment configuration for production API

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User, UserRole } from '../models/user.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl = `${environment.apiUrl}/users`;
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Record } from '../models/record.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
-  private apiUrl = 'http://localhost:3000/records';
+  private apiUrl = `${environment.apiUrl}/records`;
 
   constructor(private http: HttpClient) { }
 
