0d62575df560eba4d93ed10e71e2952d175920f7
VXSnipe <mattsheld01@gmail.com>
2026-01-16 17:27:41 +0100

    Fix Angular environment file configuration for production

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User, UserRole } from '../models/user.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
